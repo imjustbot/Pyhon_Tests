7cf8f23e344ad17a46a95482fef9f4b037faa05d
imjustbot <nico7tolu@gmail.com>
2026-06-27 22:03:49 +0000

    Aggiunto un piccolo programma formato da degli input e dei print

--- Test.py
+++ Test.py
@@ -1,1 +1,40 @@
+import random
+
+nome = input("Nome?")
+
+
+cognome = input("Cognome?")
+print(f"Ciao {nome} {cognome}!")
+
+umore = input("Come stai?")
+
+if umore == "bene" or umore == "Bene" or umore == "BENE":
+
+    print("Perfetto! sono contento che tu stia bene.")
+
+if umore == "male" or umore == "Male" or umore == "MALE":
+
+    print("Mi dispiace!")
+
+else:
+
+    print("Ok")
+
+età == input("Vabbè cambiamo discorso, quanti anni hai?")
+
+numero = int
+
+if età != numero:
+
+    print("Inserisci un numero valido!")
+
+elif età >= 18:
+
+    print("Sei maggiorenne")
+
+elif età < 18:
+
+    print("Sei maggiorenne")
+
+
 
